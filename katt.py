2cf86a396591c36a1784d22df236c78be3baa477
Yoshi哟喜 <38394135+yosiyoshi@users.noreply.github.com>
2018-04-18 17:20:10 +0900

    Update katt.py

--- katt.py
+++ katt.py
@@ -13,15 +13,18 @@ from bs4 import BeautifulSoup
 
 now = datetime.datetime.now()
 target_url = 'https://id.wikipedia.org/wiki/Stasiun_Yogyakarta'
-r = requests.get(target_url)         #get contents with requests
+r = requests.get(target_url)         #requestsを使って、webから取得
 stname = re.sub('https://id.wikipedia.org/wiki/', '', str(target_url))
-soup = BeautifulSoup(r.text, 'lxml') #use beautifulsoup
+soup = BeautifulSoup(r.text, 'lxml') #要素を抽出
 time = str(now.hour) + "."
-output = soup.find_all("td", text=re.compile(time))      #fist to find trains on time
+output = soup.find_all("td", text=re.compile(time))      #リンクを表示
 output2 = re.sub('[</td>]', '', str(output))
 output3 = output2.replace(".", ":")
-t = "Bisnis"
+t = "Ekonomi"
 c = soup.find_all("td", text=re.compile(t))
-c2 = re.sub('[(</td>);]', '', str(c))
-c3 = re.sub('amp', '', c2)
-print("Timetable at " + stname + ":\n" + output3 + "\nYou want to take " + t + " class train; \nAvailable trains stopping at " + stname + " per a day are:\n" + c3)
+c2a = re.sub('[(</=>);]', '', str(c))
+c2b = re.sub('td', '', c2a)
+c3 = re.sub('amp', '', c2b)
+c4 = re.sub('href', '', c3)
+c5 = re.sub('ile', '', c4)
+print("Timetable at " + stname + ":\n" + output3 + "\nYou want to take " + t + " class train; \nAvailable trains stopping at " + stname + " per a day are:\n" + c5)
